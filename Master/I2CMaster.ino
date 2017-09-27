0ba805bfea91a8286d499cb86ad5b9fb6a31f3d6
illes <ingbi@ille-net.de>
2017-09-27 19:32:54 +0200

    WiFi-Anbindung dazu

--- Master/I2CMaster.ino
+++ Master/I2CMaster.ino
@@ -1,18 +1,94 @@
 #include <Wire.h>
-uint8_t buffer[32];
-uint32_t startTime = 0;
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+#include <WiFiManager.h>
+#include <ESP8266HTTPClient.h>
+#include <DNSServer.h>
+#include <ArduinoJson.h>
+#include <Fs.h>
 
 #define INT_PIN D2
+#define BTN_PIN D7
+
+uint8_t buffer[32];
+uint32_t startTime = 0;
 bool dataAvailable = false;
+bool shouldSaveConfig = false;
 const int revs_per_kWh = 150;
 float counter = 0.0;
 float power = 0.0;
+char ccuip[16] = "192.168.178.54";
+char counterConstant[6];
+char ccuSysvarActPower[32];
+char ccuSysvarPowerCounter[32];
 
 struct totals
 {
    uint32_t totalPeriodTime;
    uint32_t totalRevolutions;
 };
+
+void saveConfigCallback () {
+    Serial.println("Should save config");
+    shouldSaveConfig = true;
+}
+
+bool loadConfig() { 
+    File configFile = SPIFFS.open("/config.json", "r");
+    if (!configFile) {
+        Serial.println("ERROR: failed to open config file");
+        return false;
+    }
+
+    size_t size = configFile.size();
+    if (size > 1024) {
+        Serial.println("ERROR: config file size is too large");
+        return false;
+    }
+
+    // Allocate a buffer to store contents of the file.
+    std::unique_ptr<char[]> buf(new char[size]);
+    configFile.readBytes(buf.get(), size);
+
+    StaticJsonBuffer<200> jsonBuffer;
+    JsonObject& json = jsonBuffer.parseObject(buf.get());
+
+    if (!json.success()) {
+        Serial.println("ERROR: failed to parse config file");
+        return false;
+    }
+
+    strcpy(counterConstant, json["counterConstant"]);
+    strcpy(ccuip, json["ccuip"]);
+    strcpy(ccuSysvarActPower, json["ccuSysvarActPower"]);
+    strcpy(ccuSysvarPowerCounter, json["ccuSysvarPowerCounter"]);
+
+    Serial.println("loaded configuration data: ");
+    Serial.print("counterConstant: "); Serial.println(counterConstant);
+    Serial.print("ccuip: "); Serial.println(ccuip);
+    Serial.print("ccuSysvarActPower: "); Serial.println(ccuSysvarActPower);
+    Serial.print("ccuSysvarPowerCounter: "); Serial.println(ccuSysvarPowerCounter);
+    return true;
+}
+
+void setStateCcuSysVar(String sysvar, String value) {
+    if (WiFi.status() == WL_CONNECTED)
+    {
+        HTTPClient http;
+        String url = "http://" + String(ccuip) + ":8181/cuxd.exe?ret=dom.GetObject(%22" + sysvar + "%22).State(" + value + ")";
+        Serial.println("URL = " + url);
+        http.begin(url);
+        int httpCode = http.GET();
+        Serial.println("httpcode = " + String(httpCode));
+        if (httpCode > 0) {
+            //String payload = http.getString();
+        }
+        if (httpCode != 200) {
+            Serial.println("HTTP fail " + String(httpCode));
+        }
+        http.end();
+    } else Serial.println("wifi not connected!");
+}
 
 void setThresholds(int lowThres, int uppThres) {
     Wire.beginTransmission(42);
@@ -99,11 +175,67 @@ void setup() {
   // put your setup code here, to run once:
     Serial.begin(115200);
     Serial.println();
-    
     pinMode(INT_PIN, INPUT_PULLUP);
+    pinMode(BTN_PIN, INPUT_PULLUP);
     attachInterrupt(digitalPinToInterrupt(INT_PIN), handleInterrupt, FALLING);
- 
     Wire.begin(0, 2);
+
+
+    Serial.println("starting WiFi");
+    WiFiManager wifiManager;
+    wifiManager.setSaveConfigCallback(saveConfigCallback);
+    WiFiManagerParameter custom_counterConstant("counterConstant", "Z&auml;hlerkonstante Stromz&auml;hler", counterConstant, 6);
+    WiFiManagerParameter custom_ccuip("ccu", "ip adresse der CCU", ccuip, 16);
+    WiFiManagerParameter custom_ccuSysvarActPower("ccuSysvarActPower", "Sys.-Var. f&uuml;r akt. Leistung", ccuSysvarActPower, 32);
+    WiFiManagerParameter custom_ccuSysvarPowerCounter("ccuSysvarPowerCounter", "Sys.-Var. f&uuml;r Z&auml;hlerstand", ccuSysvarPowerCounter, 32);
+    wifiManager.addParameter(&custom_counterConstant);
+    wifiManager.addParameter(&custom_ccuip);
+    wifiManager.addParameter(&custom_ccuSysvarActPower);
+    wifiManager.addParameter(&custom_ccuSysvarPowerCounter);
+
+    if (digitalRead(BTN_PIN) == LOW) {
+        Serial.println("deleting WiFi settings, starting with configuration");
+        wifiManager.resetSettings();
+    }
+    
+    wifiManager.autoConnect();
+    if (shouldSaveConfig) {
+        SPIFFS.begin();
+        Serial.println("Config changed, saving config...");
+        StaticJsonBuffer<200> jsonBuffer;
+        JsonObject& json = jsonBuffer.createObject();
+        json["counterConstant"] = custom_counterConstant.getValue();
+        json["ccuip"] = custom_ccuip.getValue();
+        json["ccuSysvarActPower"] = custom_ccuSysvarActPower.getValue();
+        json["ccuSysvarPowerCounter"] = custom_ccuSysvarPowerCounter.getValue();
+    
+        File configFile = SPIFFS.open("/config.json", "w");
+        if (!configFile) {
+            Serial.println("ERROR: failed to open config file for writing");
+        }
+        else {
+            Serial.println("actual configuration to save:");
+            json.printTo(Serial);
+            Serial.println("");
+            json.printTo(configFile);
+            configFile.close();
+            Serial.println("config file successfuly written");
+        }
+        SPIFFS.end();
+        shouldSaveConfig = false;
+    }
+
+    Serial.println("going further...");
+    Serial.println("mounting filesystem...");
+    if (!SPIFFS.begin()) {
+        Serial.println("Error mounting filesystem. This is a problem");
+    }
+    if (!loadConfig()) {
+        Serial.println("Error loading config. This is a problem");
+    }
+    
+    
+
     delay(500);
     //setThresholds(222, 777);
     //delay(500);
